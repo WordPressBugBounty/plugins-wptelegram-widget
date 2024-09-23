f9b5a06948654a4bdf871f298d8da8aed0eacb04
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 09:21:32 +0000

    2.1.28

---
+++ wptelegram-widget/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wptelegram/widget',
+        'pretty_version' => '2.1.28',
+        'version' => '2.1.28.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpsocio/wp-utils' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f7768dcc7d44830a0c58b7f6e0696bdc846781b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpsocio/wp-utils',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpsocio/wptelegram-bot-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '97410f5a4695b6eccea70e769a7aae2536ba42f2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpsocio/wptelegram-bot-api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptelegram/widget' => array(
+            'pretty_version' => '2.1.28',
+            'version' => '2.1.28.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
